9ae16918a3eedc843fecdd9d356a20c6ff9e7866
schwaebek <schwaebek@Katlyns-MacBook-Pro.local>
2014-08-06 09:17:24 -0400

    Bricks and Balls day 1

---
+++ Bricks & Balls/Bricks & Balls/BABAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BABAppDelegate.h
+//  Bricks & Balls
+//
+//  Created by Katlyn Schwaebe on 8/6/14.
+//  Copyright (c) 2014 Katlyn Schwaebe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BABAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Bricks & Balls/Bricks & BallsTests/Bricks___BallsTests.m
@@ -0,0 +1,34 @@
+//
+//  Bricks___BallsTests.m
+//  Bricks & BallsTests
+//
+//  Created by Katlyn Schwaebe on 8/6/14.
+//  Copyright (c) 2014 Katlyn Schwaebe. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Bricks___BallsTests : XCTestCase
+
+@end
+
+@implementation Bricks___BallsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
